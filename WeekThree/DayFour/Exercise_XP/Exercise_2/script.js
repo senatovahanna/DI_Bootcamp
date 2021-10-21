0053cb1e7a90c136de7af2d4bf716006484f16e8
Senatova Hanna <senatova.h@gmail.com>
2021-10-21 18:34:11 +0300

    Update exercises

--- WeekThree/DayFour/Exercise_XP/Exercise_2/script.js
+++ WeekThree/DayFour/Exercise_XP/Exercise_2/script.js
@@ -3,7 +3,7 @@ var newDog = "Chihuahua";
 	console.log(newDog.toUpperCase());
 	console.log(newDog.toLowerCase());
 
-	if (newDog === "Chihuahua") {
+	if (newDog == "Chihuahua") {
 	  console.log("I love Chihuahas, it's my favorite dog breed!");
 	} else {
 	  console.log("I don't care, I prefer cats.");
--- WeekThree/DayFour/Exercise_XP/Exercise_3/script.js
+++ WeekThree/DayFour/Exercise_XP/Exercise_3/script.js
@@ -2,6 +2,8 @@
 var num = prompt("Give me a number! Any number!");
   if (num % 2 == 0) {
     console.log(num + " is an even number");
-  } else {
+  } else if(num % 2 == 1){
     console.log(num + " is an odd number.");
+  } else{
+    console.log("your answer is not a number");
 }

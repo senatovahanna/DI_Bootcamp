967dc47c1f7237c4fe336879b49b2ae28bcdc8ca
Senatova Hanna <senatova.h@gmail.com>
2021-10-21 15:55:51 +0300

    Uploaded files with Daily Challenge of 2nd day of 3d week

---
+++ WeekThree/DayTwo/Daily_Challenge/myScript.js
@@ -0,0 +1,20 @@
+
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+	console.log(fruits); 
+fruits.shift();
+	console.log(fruits); 
+fruits.sort();
+	console.log(fruits); 
+fruits.push("Kiwi");
+	console.log(fruits); 
+fruits.splice(0, 1);
+	console.log(fruits); 
+fruits.reverse();
+	console.log(fruits); 
+
+
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+	console.log(moreFruits);
+	console.log(moreFruits[1][1][0]); 

a8509a6d1e8cc8a8a4da156429f3b86e780c7181
Senatova Hanna <senatova.h@gmail.com>
2021-10-21 15:39:44 +0300

    upload ex GuessTheUnswer of second dau of 3d week

---
+++ WeekThree/DayTwo/Exercise_XP/myscript.js
@@ -0,0 +1,42 @@
+    5 + "34"
+    // 5 is a number and "34" is a string. It should just add it (not math) , put together
+    // the answer should be "534"
+    5 - "4"
+    // My first guess was that it will be NaN because "4" is a string.
+    // But then i read that mines can change string to number, so the answer will be 1
+
+    10 % 5
+    // The answer will 0.
+
+    5 % 10
+    // The answer will 5.
+
+    "Java" + "Script"
+    // We have two strings. So the answer will be "JavaScript"
+
+    " " + " "
+    // Two strings with space will give us a string with 2 spaces
+    // "  " - the answer
+
+    " " + 0
+    // Space in string + 0, we have string and number
+    // the answer is " 0"
+
+    true + true
+    // True has a value 1, so 1+1=2
+
+    true + false
+    // True has value of 1, and false has value of 0.
+    // 1+0 is 1
+
+    false + true
+    // 1 . Math. 0+1
+
+    false - true
+    // The answer would be -1, the same, just a math.
+
+    3 - 4
+    // it should be -1 . Just a math for two numbers
+
+    "Bob" - "bill"
+    // I think it would be a NaN (not a number), because there are 2 strings, and "-" operator is only for numbers 

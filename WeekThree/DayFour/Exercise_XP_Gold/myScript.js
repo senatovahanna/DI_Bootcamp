a04e7af38d9f77e4976174d54b0a420f7e3133e7
Senatova Hanna <senatova.h@gmail.com>
2021-10-21 17:00:22 +0300

    uoloaded Exercise Gold - first ex(languages)

---
+++ WeekThree/DayFour/Exercise_XP_Gold/myScript.js
@@ -0,0 +1,16 @@
+let lang = prompt("What language do you speek?")
+lang = lang.toLowerCase()
+
+if(lang=="french") {
+	console.log("Bonjour")
+}
+else if(lang=="english") {
+	console.log("hello")
+}
+else if(lang=="hebrew") {
+	console.log("Shalom")
+}
+ else {
+ 		console.log("01110011 01101111 01110010 01110010 01111001")
+
+ }

76a57c70f3ec1f7b7e99bef184fbe0fee6c0eb02
Senatova Hanna <senatova.h@gmail.com>
2021-10-25 18:15:15 +0300

    Added Ex XP Gold to the 2d day of 3d week

---
+++ WeekThree/DayTwo/Exercise_XP_Gold/myScript.js
@@ -0,0 +1,12 @@
+// Exercise 1 : Favorite Color
+
+let me = ["my","favorite","color","is","blue"];
+me.join();
+
+
+
+//Exercise 3 : Calculator
+
+let num1 = parseInt(prompt("Write here any number"));
+let num2 = parseInt(prompt("Write here number once again, please"));
+alert(num1 + num2);

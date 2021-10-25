a24c9f005b133f655775e6a6a597390c1cc6c9e6
Senatova Hanna <senatova.h@gmail.com>
2021-10-25 17:54:36 +0300

    modify js file for 4 part of exerciseXP

--- WeekThree/DayFour/Exercise_XP/Exercise_4/script.js
+++ WeekThree/DayFour/Exercise_XP/Exercise_4/script.js
@@ -3,11 +3,11 @@
 let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 var len = users.length; 
 
-if (len == 0) {
+if (!len) {
   console.log("no one is online.");
-} else if (len == 1) {
+} else if (len === 1) {
   console.log(users[0] + " is online");
-} else if (len == 2) {
+} else if (len === 2) {
   console.log(users[0] + " and " + users[1] + " are online");
 } else {
   console.log(users[0] + " , " + users[1] + " and two more are online.");

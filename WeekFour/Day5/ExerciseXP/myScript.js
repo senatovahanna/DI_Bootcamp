718d83d5b4130598447589cf62526a8476604660
Senatova Hanna <senatova.h@gmail.com>
2021-11-05 16:52:33 +0200

    Added Ex XP

---
+++ WeekFour/Day5/ExerciseXP/myScript.js
@@ -0,0 +1,49 @@
+//Exercise 1
+
+// let navBar = document.getElementById("navBar");
+// navBar.setAttribute("id", "socialNetworkNavigation");
+
+// let node = document.createElement("li");
+// let textnode = document.createTextNode("Logout");
+// node.appendChild(textnode);
+// document.getElementById("myList").appendChild(node);
+// ----------------------------------------------------------------------------------------------------------------------
+//Exercise 2
+// let pete = document.getElementsByClassName("list")[0].lastElementChild;
+// pete.textContent = "Richard";
+// let evelyn = document.getElementsByTagName("ul")[0].firstElementChild;
+// evelyn.textContent = "Evelyn";
+// let evelyn2 = document.getElementsByTagName("ul")[1].firstElementChild;
+// evelyn2.textContent = "Evelyn";
+
+// let node = document.createElement("li");
+// let textnode = document.createTextNode("Hey Students");
+// node.appendChild(textnode);
+// document.getElementsByClassName("list")[0].appendChild(node);
+
+// let ul = document.getElementsByClassName("list2");
+// let li = document.createElement("li");
+// li.appendChild(document.createTextNode("Hey Students"));
+// ul[0].appendChild(li);
+
+// let remove = document.getElementsByTagName("li")[4];
+// remove.remove("li");
+//----------------------------------------------------------------------------------------------------------------------
+
+//Exercise 3
+// let div = document.querySelector("div");
+// div.style.background = "lightblue";
+// div.style.padding = "20px";
+
+// let x = document.getElementById("john");
+// if (x.style.display === "none") {
+//   x.style.display = "block";
+// } else {
+//   x.style.display = "none";
+// }
+
+// let pete = document.getElementsByTagName("li")[1];
+// pete.style.border = "blue solid 2px";
+
+// let body = document.querySelector("body");
+// body.style.fontSize = "30px";

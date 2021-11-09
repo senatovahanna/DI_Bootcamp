dca1420ecd64216922adbb565931f871d3502b86
Senatova Hanna <senatova.h@gmail.com>
2021-11-09 10:17:45 +0200

    finished ex XP gold

---
+++ WeekSix/Day2/exercise_Xp_GOLD.py
@@ -0,0 +1,10 @@
+#Exercise 1 : Hello World-I Love Python
+
+
+
+#Exercise 2 : What Is The Season ?
+user_choise = int(input("Choose one month (any month!) and write it's number: (januar-1, februar-2 etc..."))
+if 2 < user_choise < 6: print(f"This month belongs to Spring!")
+elif 5 < user_choise < 9: print(f"This month belongs to Summer!")
+elif 8 < user_choise < 12: print(f"This month belongs to Autumn!")
+else : print(f"This month belongs to Winter!")

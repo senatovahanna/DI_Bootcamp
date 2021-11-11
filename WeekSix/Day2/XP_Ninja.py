66f6128c46f7e74ad032b085c1663d0b8fbf147d
Senatova Hanna <senatova.h@gmail.com>
2021-11-11 11:36:08 +0200

    finished exercises XP_Ninja

---
+++ WeekSix/Day2/XP_Ninja.py
@@ -0,0 +1,16 @@
+# Exercise 4 : How Many Characters In A Sentence ?
+
+my_text = '''
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco
+           laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit
+           esse cillum dolore eu fugiat nulla pariatur.
+           Excepteur sint occaecat cupidatat non proident,
+           sunt in culpa qui officia deserunt mollit anim id est laborum.
+           '''
+my_text_length = len(my_text)
+print(f"the length of the text is {my_text_length}")
+
+

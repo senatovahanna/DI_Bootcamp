56b3d6d417eb1659081d0dda738c1780b1c6577f
Senatova Hanna <senatova.h@gmail.com>
2021-11-11 11:39:03 +0200

    added exercises of online learning day 3 week 6

---
+++ WeekSix/Day3/online_learning.py
@@ -0,0 +1,23 @@
+# # list1 = [5, 10, 15, 20, 25, 50, 20]
+# # twenty = list1.index(20)
+# # print(twenty)
+# # list1[3] = 200
+# # print(list1)
+
+
+# # aTuple = (10, 20, 30, 40)
+# # a, b, c, d = aTuple
+
+# numbers = range(1, 11)
+
+# for number in numbers:
+#     print(number)
+
+# # or
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+# print("Finished")
+
